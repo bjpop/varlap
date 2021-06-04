36397867d7efd51b2215b37f3ae36235a5f83ea9
Bernie Pope <bjpope@unimelb.edu.au>
2021-06-04 17:13:33 +1000

    Allow optional variant input filename to be specified on the command line

--- varlap/varlap.py
+++ varlap/varlap.py
@@ -73,7 +73,9 @@ def parse_args():
     parser.add_argument(
         '--sample', default='', required=False, metavar='SAMPLE', type=str, help='Sample identifier')
     parser.add_argument(
-        '--regions', required=False, metavar='REGIONS', type=str, help='Filepath of genomic regions-of-interest in BED format')
+        '--vars', required=False, metavar='FILE', type=str, help=f'Optional filepath of input variants. Allowed formats: {",".join(VALID_INPUT_FILE_FORMATS)}')
+    parser.add_argument(
+        '--regions', required=False, metavar='REGIONS', type=str, help='Optional filepath of genomic regions-of-interest in BED format')
     parser.add_argument(
         '--format', required=False, metavar='FORMAT', type=str, default=DEFAULT_INPUT_FILE_FORMAT, choices=VALID_INPUT_FILE_FORMATS,
         help='File format of input variants file. Options: %(choices)s. Default: %(default)s')
@@ -370,11 +372,25 @@ def get_chrom_pos_fraction(readers, variant):
 def variant_as_list(variant, fieldnames):
     return [str(variant[f]) for f in fieldnames]
 
+
+def get_variant_reader(options):
+    if options.vars is not None:
+        try:
+           input_file = open(options.vars)
+        except Exception as e:
+           print(e)
+           exit_with_error(f"Input variant file does not exist or could not be opened: {options.vars}", EXIT_FILE_IO_ERROR)
+    else:
+       input_file = sys.stdin
+    variant_reader = VariantReader(input_file, options.format, options.varclass, options.maxindelsize)
+    return variant_reader
+
+
 def process_variants_bams(options, regions):
     writer = csv.writer(sys.stdout)
     bam_labels = get_bam_labels(options.labels, options.bams)
     bam_readers = [BamReader(filepath, options.varclass) for filepath in options.bams]
-    variant_reader = VariantReader(sys.stdin, options.format, options.varclass, options.maxindelsize)
+    variant_reader = get_variant_reader(options)
     write_header(writer, options, variant_reader.fieldnames, bam_labels, regions)
     for variant in variant_reader.get_variants():
         pos_normalised = get_chrom_pos_fraction(bam_readers, variant)
@@ -806,6 +822,7 @@ def get_regions(filepath):
 
 
 def main():
+    breakpoint()
     "Orchestrate the execution of the program"
     options = parse_args()
     init_logging(options.log)
