d7dc47600e29fc031795bec5318462c983d7d9e6
Jiayu Wang <jiayu@spartan-login1.hpc.unimelb.edu.au>
2021-06-12 17:51:53 +1000

    withraw the change on chr1

--- varlap/varlap.py
+++ varlap/varlap.py
@@ -758,7 +758,7 @@ class BamReader(object):
 
     # pos is expected to be 1-based 
     def variant_features(self, variant):
-        chrom = str(variant["chrom"])[3:]
+        chrom = variant["chrom"]
         pos = variant["pos"]
         ref = variant["ref"]
         alt = variant["alt"]
